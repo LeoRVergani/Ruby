3ba6967d3fa35c80647190e03c5334e305254e1c
leorvergani <vergani@outlook.com.br>
2024-06-23 22:22:14 -0300

    Hello World

---
+++ hello.rb
@@ -0,0 +1,17 @@
+puts('Digite um número')
+number = gets.chomp().to_i
+
+# Operador condicional
+if number.positive?
+  puts("O número #{number} é positivo")
+end
+if number.negative?
+  puts("O número #{number} é negativo")
+end
+if number.even?
+  puts("O número #{number} é par")
+end
+if number.odd?
+  puts("O número #{number} é impar")
+end
+puts("Tudo que é digitado é uma #{number.class}")
