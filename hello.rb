d0274c8b4148f501b877583b9cfdea1d75cb73a7
leorvergani <vergani@outlook.com.br>
2024-06-23 22:24:14 -0300

    update

--- hello.rb
+++ hello.rb
@@ -3,15 +3,15 @@ number = gets.chomp().to_i
 
 # Operador condicional
 if number.positive?
-  puts("O número #{number} é positivo")
+    puts("O número #{number} é positivo")
 end
 if number.negative?
-  puts("O número #{number} é negativo")
+    puts("O número #{number} é negativo")
 end
 if number.even?
-  puts("O número #{number} é par")
+    puts("O número #{number} é par")
 end
 if number.odd?
-  puts("O número #{number} é impar")
+    puts("O número #{number} é impar")
 end
 puts("Tudo que é digitado é uma #{number.class}")
